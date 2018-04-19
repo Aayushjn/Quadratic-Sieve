7ad86b3f87cf145de4f93d359bc1e82ee022b68c
Aayushjn <aayushjn11@gmail.com>
2018-04-19 10:58:19 +0530

    Revamped code

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Quadratic sieve implementation in Python
 
 Run 'main.py' to begin execution
+Try the numbers given in 'Numbers.txt' to run Pollard rho
--- src/main.py
+++ src/main.py
@@ -1,24 +1,58 @@
 from math import fabs, ceil, sqrt, exp, log
-import random
-from MillerRabin import is_probable_prime
 from shanks import STonelli
 from itertools import chain
+from random import randint
+import helpers
 
-def gcd(a,b): # Euclid's algorithm
-    if b == 0:
-        return a
-    elif a >= b:
-        return gcd(b,a % b)
-    else:
-        return gcd(b,a)
+def is_probable_prime(a):
+    """ Perform Rabin-Miller primality test to determine whether given number
+        is prime. Return True if number is very likely to be a prime, and False
+        if it is definitely composite
+    """
+    if a == 2:
+        return True
 
-def isqrt(n): # Newton's method, returns exact int for large squares
-    x = n
-    y = (x + 1) // 2
-    while y < x:
-        x = y
-        y = (x + n // x) // 2
-    return x
+    if a == 1 or a % 2 == 0:
+        return False
+
+    return rabin_miller_primality_test(a, 50)
+
+def rabin_miller_primality_test(a, iterations):
+    """ Rabin Miller primality test
+    """
+    r, s = 0, a - 1
+
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+
+    for _ in range(iterations):
+        n = randint(2, a - 1)
+        x = pow(n, s, a)
+        if x == 1 or x == a - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, a)
+            if x == a - 1:
+                break
+        else:
+            return False
+    return True
+
+def check_perfect_power(n):
+    """ Check if the given integer is a perfect power. If yes, return (r, b)
+        such that r^b = n, otherwise return None.
+        Assume that global small_primes has already been initialised and that n
+        does not have any prime factors from small_primes.
+    """
+    prime = small_primes[-1]
+    for p in small_primes:
+        pth_root = helpers.kth_iroot(n, p)
+        if pth_root < prime:
+            break
+        if pth_root ** p == n:
+            return (pth_root, p)
+    return None
 
 def mprint(M): #prints a matrix in readable form
     for row in M:
@@ -73,10 +107,6 @@ def quad_residue(a,n):
             z=(z*a) % n
 
     return z
-
-
-
-
         
 def size_bound(N): # finds optimal factor base size and interval
 
@@ -86,7 +116,6 @@ def size_bound(N): # finds optimal factor base size and interval
     return int(F),int(I)
 
 
-    
 def find_base(N,B):
 # generates a B-smooth factor base
 
@@ -260,9 +289,9 @@ def solve(solution_vec,smooth_nums,xlist,N):
     for n in x_nums:
         b *= n
 
-    a = isqrt(Asquare)
+    a = helpers.isqrt(Asquare)
     
-    factor = gcd(b-a,N)
+    factor = helpers.gcd(b-a,N)
     return factor
 
 
@@ -278,7 +307,7 @@ def QS(n,B,I):
         return "prime"
     
     if isinstance(sqrt(N),int):
-        return isqrt(N)
+        return helpers.isqrt(N)
     
     #print(root)
     print("Attempting to factor {}...".format(N))
@@ -308,7 +337,7 @@ def QS(n,B,I):
     
     if is_square == True:
         x = smooth_nums.index(t_matrix)
-        factor = gcd(xlist[x]+sqrt(t_matrix),N)
+        factor = helpers.gcd(xlist[x]+sqrt(t_matrix),N)
         print("Found a square!")
         return factor, N/factor
     
@@ -336,6 +365,240 @@ def QS(n,B,I):
             
             
     return("Didn't find any nontrivial factors!")
-                   
 
-print(QS(1811706971,1000,1000))    
+def check_factor(n, i, factors):
+    """ Checks whether 'i' is a factor of 'n' and adds 'i' to 'factors' if true
+        by trial division
+    """
+    while n % i == 0:
+        n //= i
+        factors.append(i)
+        if is_probable_prime(n):
+            factors.append(n)
+            n = 1
+    return n
+
+def find_small_primes(n, upper_bound):
+    """ Perform trial division on the given number using all the primes up
+        to the upper bound. Initialize the global variable 'small_primes' with
+        a list of all the primes <= upper_bound.
+    """
+    print("Trial division and initializing small primes...")
+    global small_primes
+    is_prime = [True] * (upper_bound + 1)
+    is_prime[0:2] = [False] * 2
+    factors = []
+    small_primes = []
+    max_i = helpers.isqrt(upper_bound)
+    rem = n
+    for i in range(2, max_i + 1):
+        if is_prime[i]:
+            small_primes.append(i)
+            rem = check_factor(rem, i, factors)
+            if rem == 1:
+                return factors, 1
+
+            for j in range(i ** 2, upper_bound + 1, i):
+                is_prime[j] = False
+
+    for i in range(max_i + 1, upper_bound + 1):
+        if is_prime[i]:
+            small_primes.append(i)
+            rem = check_factor(rem, i, factors)
+            if rem == 1:
+                return factors, 1
+
+    print("Primes initialised!")
+    return factors, rem
+
+def find_prime_factors(n):
+    """ Return one or more prime factors of the given number n.
+        Assume that n is not a prime and does not have very small factors, and
+        that the global small_primes has already been initialised. Do not
+        return duplicate factors.
+    """
+    print("Checking whether {} is a perfect power ...".format(n))
+    perfect_power = check_perfect_power(n)
+    if perfect_power:
+        print("{} is {}^{}".format(n, perfect_power[0], perfect_power[1]))
+        factors = perfect_power[0]
+    else:
+        print("Not a perfect power")
+        digits = len(str(n))
+        print("Using Brent's variant of Pollard's rho factorization " + \
+                "algorithm to factorise {} ({} digits)".format(n, digits))
+        factors = [brent_factorise(n)]
+
+    prime_factors = []
+    for f in set(factors):
+        for pf in find_all_prime_factors(f):
+            prime_factors.append(pf)
+
+    return prime_factors
+
+def find_all_prime_factors(n):
+    """ Return all prime factors of the given number n.
+        Assume that n does not have very small factors and that the global
+        small_primes has already been initialised.
+    """
+    rem = n
+    factors = []
+
+    while rem > 1:
+        if is_probable_prime(rem):
+            factors.append(rem)
+            break
+
+        for f in find_prime_factors(rem):
+            print("Prime factor found: {}".format(f))
+            assert is_probable_prime(f)
+            assert rem % f == 0
+            while rem % f == 0:
+                rem //= f
+                factors.append(f)
+
+    return factors
+
+def _pollard_brent_func(c, n, x):
+    """ Return f(x) = (x^2 + c) % n
+        Assume c < n
+    """
+    y = (x ** 2) % n + c
+    if y >= n:
+        y -= n
+
+    assert y >= 0 and y < n
+    return y
+
+def brent_factorise(n, iterations=None):
+    """ Perform Brent's variant of Pollard's rho factorization algorithm to
+        attempt to find a non-trivial factor of the given number number, n.
+        If iterations > 0, return None if no factors are found within its range
+    """
+    y, c, m = (randint(1, n - 1) for _ in range(3))
+    r, q, g = 1, 1, 1
+    i = 0
+    while g == 1:
+        x = y
+        for _ in range(r):
+            y = _pollard_brent_func(c, n, y)
+        k = 0
+        while k < r and g == 1:
+            ys = y
+            for _ in range(min(m, r - k)):
+                y = _pollard_brent_func(c, n, y)
+                q = (q * abs(x - y)) %  n
+            g = helpers.gcd(q, n)
+            k += m
+        r *= 2
+        if iterations:
+            i += 1
+            if i == iterations:
+                return None
+
+    if g == n:
+        while True:
+            ys = _pollard_brent_func(c, n, ys)
+            g = helpers.gcd(abs(x - ys), n)
+            if g > 1:
+                break
+    return g
+
+def pollard_brent_iterator(n, factors):
+    """ Iterator function for Brent's variant of Pollard's rho factorization
+        algorithm to find all small prime factors. Restart every time a factor
+        is found.
+        Return 1 if all prime factors are found, or otherwise the remaining
+        factor
+    """
+    rem = n
+    while True:
+        if is_probable_prime(n):
+            factors.append(n)
+            rem = 1
+            break
+
+        digits = len(str(n))
+        if digits < 30:
+            iterations = 20
+        else:
+            iterations = 25
+
+            f = brent_factorise(rem, iterations)
+            if f and f < rem:
+                if is_probable_prime(f):
+                    print("Brent's (Pollard's rho): Prime factor found: " + \
+                        "{}".format(f))
+                    factors.append(f)
+                    rem //= f
+                else:
+                    print("Brent's (Pollard's rho): Composite factor " + \
+                          "found: {}".format(f))
+                    rem_f = pollard_brent_iterator(f, factors)
+                    rem = (rem // f) * rem_f
+            else:
+                print("No more small factors found")
+                break
+    return rem
+
+def factorise(n):
+    if type(n) != int or n < 1:
+        raise ValueError("Number must be a POSITIVE INTEGER")
+
+    print("Factorizing {} ({} digits)...".format(n, len(str(n))))
+
+    if n == 1:
+        return []
+
+    if is_probable_prime(n):
+        return [n]
+
+    factors, rem = find_small_primes(n, 10000)
+
+    if factors:
+        print("Prime factors found so far:")
+        factors_temp = []
+        for _ in factors:
+            if _ not in factors_temp:
+                factors_temp.append(_)
+        print(*factors_temp, sep=', ')
+    else:
+        print("No small factors found!")
+
+    if rem != 1:
+        digits = len(str(rem))
+        if digits > 30:
+            print("Attempting Quick Pollard's rho (Brent's variation) to " + \
+                  "find slightly larger factors...")
+            rem = pollard_brent_iterator(rem, factors)
+        if rem > 1:
+            for f in find_all_prime_factors(rem):
+                factors.append(f)
+
+    factors.sort()
+    assert helpers.product(factors) == n
+    for p in factors:
+        assert is_probable_prime(p)
+    return factors
+                   
+def main():
+    n = int(input("Enter the number to be factorized: "))
+
+    if len(str(n)) > 50:
+        print("Using Quadratic Sieve to factorise {} ({} digits)".format(n, len(str(n))))
+        #B, interval = int(input("Enter B-value and interval: "))
+        #print(QS(n, B, i))
+        print(QS(1811706971,1000,1000))
+    else:
+        result = factorise(n)
+        new_result = []
+
+        for _ in result:
+            if _ not in new_result:
+                new_result.append(_)
+
+        print("\nPrime factors: {}".format(new_result))
+
+if __name__ == '__main__':
+    main()
+    
